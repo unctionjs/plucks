b7d0dda1cc69f4fad746ccccbe970c5295e4da3d
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:06:18 -0700

    Don't bother using the extension, eslint complains

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-magic-numbers */
 import {test} from "tap"
 
-import plucks from "./source.js"
+import pluck from "./source"
 
 test(({same, end}) => {
   same(
