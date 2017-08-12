0d8f413fe045a511e8f5d35c660744ae3ba5d3c3
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:11:18 -0700

    Woops, totally fucked up these paths

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-magic-numbers */
 import {test} from "tap"
 
-import pluck from "./source"
+import plucks from "./source"
 
 test(({same, end}) => {
   same(
