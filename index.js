4707f5cae41a026c60307186c92835311230e68b
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-27 03:27:49 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-import dig from "@unction/dig"
-import {juxt} from "ramda"
-import mapValues from "@unction/mapvalues"
-
-import type {ArrayType} from "types"
-import type {KeyChainType} from "types"
-import type {UnaryFunctionType} from "types"
-
-export default function plucks (keychains: ArrayType<KeyChainType>): UnaryFunctionType {
-  return mapValues(juxt(mapValues(dig)(keychains)))
+import dig from "@unction/dig";
+import { juxt } from "ramda";
+import mapValues from "@unction/mapvalues";
+export default function plucks(keychains) {
+  return mapValues(juxt(mapValues(dig)(keychains)));
 }
